fb082710d9b45d80b62396843f4dd62d181f2525
azeez-abp <adioazeezadeyori@gmail.com>
2024-01-21 19:01:07 -0800

    qk fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,70 @@
 #include "sort.h"
+
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, ssize_t item1, ssize_t item2)
-{
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-/**
- *lomuto_partition - lomuto partition sorting scheme implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: size array
- *Return: return the position of the last element sorted
- */
-int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
-{
-	int pivot = array[last];
-	ssize_t current = first, finder;
-
-	for (finder = first; finder < last; finder++)
-	{
-		if (array[finder] < pivot)
-		{
-			if (array[current] != array[finder])
-			{
-				swap(array, current, finder);
-				print_array(array, size);
-			}
-			current++;
-		}
-	}
-	if (array[current] != array[last])
-	{
-		swap(array, current, last);
-		print_array(array, size);
-	}
-	return (current);
-}
-/**
- *qs - qucksort algorithm implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: array size
- */
-void qs(int *array, ssize_t first, ssize_t last, int size)
-{
-	ssize_t position = 0;
-
-
-	if (first < last)
-	{
-		position = lomuto_partition(array, first, last, size);
-
-		qs(array, first, position - 1, size);
-		qs(array, position + 1, last, size);
-	}
-}
-/**
- *quick_sort - prepare the terrain to quicksort algorithm
- *@array: array
- *@size: array size
- */
+ * quick_sort - Sorts integers using Quick sort algorithm.
+ * @array: List of data
+ * @size: Size of the array
+**/
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (size < 2)
 		return;
-	qs(array, 0, size - 1, size);
+
+	partition_caller(array, 0, size - 1, size);
+}
+
+/**
+ * partition_caller - Calls partition to find sides and quicksort them
+ * @array: List of data
+ * @left: Starting index
+ * @right: Ending index
+ * @size: Size of the array
+**/
+void partition_caller(int *array, int left, int right, size_t size)
+{
+	int pivot = 0;
+
+	if (left < right)
+	{
+		pivot = partition(array, left, right, size);
+		partition_caller(array, left, pivot - 1, size);
+		partition_caller(array, pivot + 1, right, size);
+	}
+}
+
+/**
+ * partition - Divides the array with pivot at the center
+ * @array: List of data
+ * @left: Starting index
+ * @right: Ending index
+ * @size: Size of the array
+ * Return: Returns index of the pivot
+**/
+int partition(int *array, int left, int right, size_t size)
+{
+	int i = left - 1;
+	int pivot = array[right];
+	int j = 0, tmp = 0;
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	if (pivot < array[i + 1])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[right];
+		array[right] = tmp;
+		if (i + 1 != j)
+			print_array(array, size);
+	}
+	return (i + 1);
 }
